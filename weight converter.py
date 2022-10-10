9df0e21510d31df1c9bbfdb348d222765a82f886
michael <michaelmatere606@gmail.com>
2022-10-10 06:49:49 -0400

    weight converter

---
+++ weight converter.py
@@ -0,0 +1,4 @@
+weight_in_pounds = input('Enter you weight(pounds): ')
+weight_in_kgs = float(weight_in_pounds) * 0.4535
+string_weight_in_kgs = str(weight_in_kgs)
+print('You weigh '+string_weight_in_kgs+' kgs')
